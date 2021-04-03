0a1f7ead0f84921169f677cac8bf652eb60b999c
engnour94 <nour.abuelenein@hotmail.com>
2021-04-03 16:12:16 +0300

    done

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { request, response } = require('express');
 const express= require('express');
 const server = express();
 const PORT=3030;
-// const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3030;
 server.get('/test',(request,response)=>{
     response.send('your server is alive ')
 })
