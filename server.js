0c6f230feb63c110e8f6d9b9f17f04d90e1aa5ce
engnour94 <nour.abuelenein@hotmail.com>
2021-04-03 21:20:18 +0300

    done

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ server.use(express.static('./public'));
 server.get('/test',(request,response)=>{
     response.send('your server is alive ')
 })
-
 server.get('/',(req,res)=>{
 res.send('home route')
 })
@@ -14,18 +13,3 @@ server.listen(PORT,()=>{
     console.log(`listening on port${PORT}`)
 })
 
-// 'use strict';
-// const express = require('express');
-// const server = express();
-// const PORT = process.env.PORT || 3030;
-// server.use(express.static('./public'));
-// server.get('/',(req,res)=>{
-//     res.send('home route')
-// })
-// server.get('/test',(request,response)=>{
-//     response.send('You server is alive!!')
-// })
-
-// server.listen (PORT,()=>{
-//     console.log(`Listening on PORT ${PORT}`);
-// })
